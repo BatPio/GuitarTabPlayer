b4d50e7c59ec1f01455b8f846f811d1dedb19da3
BatPio <prbator@gmail.com>
2017-09-30 18:39:37 +0200

    Content Security Policy

--- appinfo/app.php
+++ appinfo/app.php
@@ -13,3 +13,9 @@ namespace OCA\GuitarTabPlayer\AppInfo;
 use OCP\Util;
 
 Util::addScript('guitartabplayer', 'filesPlugin');
+if(class_exists('\\OCP\\AppFramework\\Http\\EmptyContentSecurityPolicy')) {
+    $manager = \OC::$server->getContentSecurityPolicyManager();
+    $csp = new \OCP\AppFramework\Http\EmptyContentSecurityPolicy();
+    $csp->addAllowedChildSrcDomain('\'self\'');
+    $manager->addDefaultPolicy($csp);
+}
